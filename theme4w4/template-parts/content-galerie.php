9ff9ca3a6de42386867bf717df5ef4a3868a64bb
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-04-17 19:42:13 -0400

    intégration de l'animation 'Card Flip'

--- theme4w4/template-parts/content-galerie.php
+++ theme4w4/template-parts/content-galerie.php
@@ -10,9 +10,13 @@ global $tPropriété;
 ?>
 
 <article>
+    <div class="interieur">
+<div class="imageavant">
 <a href="<?php echo get_permalink() ?>"><?php the_post_thumbnail( 'thumbnail'); ?></a>
+</div>
 <div class="galerie_info">
 <p><?php the_title(); ?></p>
 <p><?php the_author(); ?></p>
+</div>
 </div>
 </article> 
