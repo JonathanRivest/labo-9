76a445d5cda8c373ce703b546e29b69322d55bb5
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-04-28 23:44:26 -0400

    Labo 8

--- theme4w4/category-projet.php
+++ theme4w4/category-projet.php
@@ -44,5 +44,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
--- theme4w4/front-page.php
+++ theme4w4/front-page.php
@@ -43,18 +43,26 @@ get_header();
 						<?php endif; ?>
 					<?php endif; ?>	
 					<h2><?php echo $tPropriété['typeCours'] ?></h2>
-					<section <?php echo (in_array($tPropriété['typeCours'], ['Web', 'Jeu', 'Spécifique']) ? 'class="carrousel-2"':'class="bloc"'); ?>>
+					<section  <?php echo (in_array($tPropriété['typeCours'], ['Web', 'Jeu', 'Spécifique']) ? 'class="carrousel-2"':'class="bloc"'); ?>>
 				<?php endif ?>	
 
 				<?php if (in_array($tPropriété['typeCours'], ['Web', 'Jeu', 'Spécifique']) ) : 
 						get_template_part( 'template-parts/content', 'cours-carrousel' ); 
 						$chaine_bouton_radio .= '<input class="rad-carrousel"  type="radio" name="rad-'.$tPropriété['typeCours'].'">';
+						elseif ($tPropriété['typeCours'] == 'Projet'):
+							get_template_part( 'template-parts/content', 'galerie' ); 
+
 				else :		
 						get_template_part( 'template-parts/content', 'cours-article' ); 
 				endif;	
 				$precedent = $tPropriété['typeCours'];
 			endwhile;?>
 			</section> <!-- fin section cours -->
+			<section class="nouvelles">
+			<button id="bout_nouvelles">Dernières Nouvelles</button>
+			<section></section>
+
+			</section>
 		<?php endif; ?>
 
 
@@ -63,7 +71,7 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
 
 function convertirTableau(&$tPropriété)
