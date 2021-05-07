7bd3e7cdd68cebde208dde94f32ce0045d246d95
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-05-06 22:05:12 -0400

    Debut labo 9

--- theme4w4/template-parts/content.php
+++ theme4w4/template-parts/content.php
@@ -11,6 +11,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+
+	<h2>///// mon article content.php</h2>
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
